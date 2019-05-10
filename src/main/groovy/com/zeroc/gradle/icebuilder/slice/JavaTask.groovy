fecb2dff2e6afd3665e1c778c290c659c93cf272
Riad Gozim <rgozim1@gmail.com>
2019-05-10 18:52:11 +0100

    Simplified java task

--- src/main/groovy/com/zeroc/gradle/icebuilder/slice/JavaTask.groovy
+++ src/main/groovy/com/zeroc/gradle/icebuilder/slice/JavaTask.groovy
@@ -1,6 +1,6 @@
 package com.zeroc.gradle.icebuilder.slice
 
-import org.apache.commons.io.FilenameUtils
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.ConfigurableFileCollection
@@ -12,9 +12,6 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.incremental.IncrementalTaskInputs
-
-import java.nio.file.Path
 
 class JavaTask extends DefaultTask {
 
@@ -28,7 +25,7 @@ class JavaTask extends DefaultTask {
     @Optional
     final Property<Boolean> impl = project.objects.property(Boolean)
 
-    @InputFiles
+    @Input
     @Optional
     final ConfigurableFileCollection includeDirs = project.files()
 
@@ -42,66 +39,35 @@ class JavaTask extends DefaultTask {
     SliceExtension sliceExt = project.slice
 
     @TaskAction
-    void action(IncrementalTaskInputs inputs) {
-        if (!inputs.incremental) {
-            project.delete(outputDir.asFile.get().listFiles())
-        }
+    void action() {
+        List<String> cmd = [sliceExt.slice2java, "-I" + sliceExt.sliceDir]
 
-        List<String> filesForProcessing = []
-        inputs.outOfDate { change ->
-            if (change.file.directory) return
-
-            // Log which file will be included in slice2py
-            Log.info("File for processing: $change.file")
-
-            // Add input file for processing
-            filesForProcessing.add(String.valueOf(change.file))
-        }
-
-        if (!filesForProcessing.isEmpty()) {
-            List<String> cmd = ["slice2java", "-I" + sliceExt.sliceDir]
-
-            if (!includeDirs.isEmpty()) {
-                // Add any additional includes
-                includeDirs.files.each { File dir -> cmd.add("-I" + dir) }
+        if (!includeDirs.isEmpty()) {
+            // Add any additional includes
+            includeDirs.files.each { File file ->
+                if (file.isDirectory()) {
+                    cmd.add("-I" + file)
+                }
             }
-
-            // Add files for processing
-            cmd.addAll(filesForProcessing)
-
-            if (tie.getOrElse(false)) {
-                cmd.add("--tie")
-            }
-
-            if (impl.getOrElse(false)) {
-                cmd.add("--impl")
-            }
-
-            // Set the output directory
-            cmd.add("--output-dir=" + outputDir.asFile.get())
-            executeCommand(cmd)
         }
 
-        inputs.removed { change ->
-            if (change.file.directory) return
-
-            deleteOutputFile(change.file)
+        sourceFiles.files.each { File file ->
+            cmd.add(String.valueOf(file))
         }
+
+        if (tie.getOrElse(false)) {
+            cmd.add("--tie")
+        }
+
+        if (impl.getOrElse(false)) {
+            cmd.add("--impl")
+        }
+
+        cmd.add("--output-dir=" + outputDir.asFile.get())
+        executeCommand(cmd)
     }
 
-    void deleteOutputFile(File file) {
-        Path resolvedPath = outputDir.get().asFile.toPath().resolve(file.toPath())
-        File resolvedFile = resolvedPath.toFile()
-
-        // Convert the input filename to the output filename and
-        // delete that file
-        File targetFile = new File(resolvedFile.path, FilenameUtils.getBaseName(resolvedFile.name) + ".java")
-        if (targetFile.exists()) {
-            targetFile.delete()
-        }
-    }
-
-    private void executeCommand(List cmd) {
+    private void executeCommand(List<String> cmd) {
         StringBuffer sout = new StringBuffer()
         Process p = cmd.execute()
         p.waitForProcessOutput(sout, System.err)
