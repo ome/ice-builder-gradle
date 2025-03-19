55081b6bde74b8a2ee0a02e52c94cafa212df83b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2025-03-19 17:25:27 +0000

    Add missing import

--- src/main/groovy/com/zeroc/gradle/icebuilder/slice/IceDocsTask.groovy
+++ src/main/groovy/com/zeroc/gradle/icebuilder/slice/IceDocsTask.groovy
@@ -10,6 +10,7 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
